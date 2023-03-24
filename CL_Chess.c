6121f5304f14f19908cb3aa48a6ead3df40f8f2c
harsh banthiya <hbanthiya@gmail.com>
2023-03-24 17:01:26 -0400

    I can generate all the moves and update movebuff

--- CL_Chess.c
+++ CL_Chess.c
@@ -1,5 +1,193 @@
 #include "CL_Chess.h"
 
+// Abstracted Move Generator 
+void    gen_push(int from, int dest, int type, MOVE *pBuf, int *pMCount)
+{
+    MOVE    move;
+    move.from = from;
+    move.dest = dest;
+    move.type = type;
+    // Update the moves buffer
+    pBuf[*pMCount] = move;
+    *pMCount = *pMCount + 1;
+}
+
+void    gen_push_normal(int from, int dest, MOVE *pBuf, int *pMCount)
+{
+    gen_push(from, dest, MOVE_TYPE_NORMAL, pBuf, pMCount);
+}
+
+// Pawn moves generate with promotion 
+void    gen_push_pawn(int from, int dest, MOVE *pBuf, int *pMCount)
+{
+    if (dest > 7 && dest < 56)
+        gen_push(from, dest, MOVE_TYPE_NORMAL, pBuf, pMCount);
+    else 
+    {
+        gen_push(from, dest, MOVE_TYPE_PROMOTION_TO_QUEEN, pBuf, pMCount);
+        gen_push(from, dest, MOVE_TYPE_PROMOTION_TO_ROOK, pBuf, pMCount);
+        gen_push(from, dest, MOVE_TYPE_PROMOTION_TO_BISHOP, pBuf, pMCount);
+        gen_push(from, dest, MOVE_TYPE_PROMOTION_TO_KNIGHT, pBuf, pMCount);
+    }
+}
+
+// Generate all the moves of current_side to move and push them to pBuf and return number of moves 
+int generate_all_moves(int current_side, MOVE *pBuf)
+{
+    int     i, k, y, row, col, movecount;
+    movecount = 0;
+
+    for (i = 0; i < 64; i++)
+    {
+        if (color[i] == current_side)
+        {
+            switch (board[i])
+            {
+                case PAWN:
+                    col = COL(i); 
+                    row = ROW(i);
+                    if (current_side == BLACK)
+                    {
+                        if (color[i + ONE_RANK] == EMPTY) // check if there is an empty square infront of pawn's current rank
+                            gen_push_pawn(i, i + ONE_RANK, pBuf, &movecount);
+                        if (row == 1 && color[i + ONE_RANK] == EMPTY && color[i + TWO_RANKS] == EMPTY) // check if pawn is at starting rank and two sqaures ahead are empty
+                            gen_push_normal(i, i + TWO_RANKS, pBuf, &movecount);
+                        if (col && color[i + 7] == WHITE) // This if and the next if check if Whites pieces are either on diagonal left and right and available to capture
+                            gen_push_normal(i, i + 7, pBuf, &movecount);
+                        if (col < 7 && color[i + 9] == WHITE)
+                            gen_push_normal(i, i + 9, pBuf, &movecount);
+                    }
+                    else // WHITE ; Same logic with different offsets and starting ranks 
+                    {
+                        if (color[i - ONE_RANK] == EMPTY) // check if there is an empty square infront of pawn's current rank
+                            gen_push_pawn(i, i - ONE_RANK, pBuf, &movecount);
+                        if (row == 6 && color[i - ONE_RANK] == EMPTY && color[i - TWO_RANKS] == EMPTY) // check if pawn is at starting rank and two sqaures ahead are empty
+                            gen_push_normal(i, i - TWO_RANKS, pBuf, &movecount);
+                        if (col && color[i - 9] == BLACK) // This if and the next if check if Black pieces are either on diagonal left and right and available to capture
+                            gen_push_normal(i, i - 9, pBuf, &movecount);
+                        if (col < 7 && color[i - 7] == BLACK)
+                            gen_push_normal(i, i - 7, pBuf, &movecount);
+                    }
+                break;
+                case QUEEN: // Do not need additional logic Queen behaves like bishop + rook, so we let it fall through
+                case BISHOP:
+                    for (y = i - 9; y >= 0 && COL(y) != 7; y -= 9) // Go left up
+                    {
+                        if (color[y] != current_side)
+                            gen_push_normal(i, y, pBuf, &movecount);
+                        if (color[y] != EMPTY)
+                            break ;
+                    }
+                    for (y = i - 7; y >= 0 && COL(y) != 0; y -= 7) // Go right up
+                    {
+                        if (color[y] != current_side)
+                            gen_push_normal(i, y, pBuf, &movecount);
+                        if (color[y] != EMPTY)
+                            break ;
+                    }
+                    for (y = i + 9; y < 64 && COL(y) != 0; y += 9) // Go left down
+                    {
+                        if (color[y] != current_side)
+                            gen_push_normal(i, y, pBuf, &movecount);
+                        if (color[y] != EMPTY)
+                            break ;
+                    }
+                    for (y = i + 7; y < 64 && COL(y) != 7; y += 7) // Go left down
+                    {
+                        if (color[y] != current_side)
+                            gen_push_normal(i, y, pBuf, &movecount);
+                        if (color[y] != EMPTY)
+                            break ;
+                    }
+                    if (board[i] == BISHOP) // this is to let it fall through for the queen
+                        break ;
+                case ROOK:
+                    col = COL(i);
+                    for (k = i - col, y = i - 1; y >= k; y--) // Go left
+                    {
+                        if (color[y] != current_side)
+                            gen_push_normal(i, y, pBuf, &movecount);
+                        if (color[y] != EMPTY)
+                            break ;
+                    }
+                    for (k = i - col + 7, y = i + 1; y <= k; y++) // Go right
+                    {
+                        if (color[y] != current_side)
+                            gen_push_normal(i, y, pBuf, &movecount);
+                        if (color[y] != EMPTY)
+                            break ;
+                    }
+                    for (y = i - 8; y >= 0; y -= 8) // Go up
+                    {
+                        if (color[y] != current_side)
+                            gen_push_normal(i, y, pBuf, &movecount);
+                        if (color[y] != EMPTY)
+                            break ;
+                    }
+                    for (y = i + 8; y < 64 ; y += 8) // Go down
+                    {
+                        if (color[y] != current_side)
+                            gen_push_normal(i, y, pBuf, &movecount);
+                        if (color[y] != EMPTY)
+                            break ;
+                    }
+                break ;
+                case KNIGHT:
+                    col = COL(i);
+                    y = i - 6;
+                    if (y >= 0 && col < 6 && color[y] != current_side)
+                        gen_push_normal(i, y, pBuf, &movecount);
+                    y = i - 10;
+                    if (y >= 0 && col > 1 && color[y] != current_side)
+                        gen_push_normal(i, y, pBuf, &movecount);
+                    y = i - 15;
+                    if (y >= 0 && col < 7 && color[y] != current_side)
+                        gen_push_normal(i, y, pBuf, &movecount);
+                    y = i - 17;
+                    if (y >= 0 && col > 0 && color[y] != current_side)
+                        gen_push_normal(i, y, pBuf, &movecount);
+                    y = i + 6;
+                    if (y < 64 && col > 1 && color[y] != current_side)
+                        gen_push_normal(i, y, pBuf, &movecount);
+                    y = i + 10;
+                    if (y < 64 && col < 6 && color[y] != current_side)
+                        gen_push_normal(i, y, pBuf, &movecount);
+                    y = i + 15;
+                    if (y < 64 && col > 0 && color[y] != current_side)
+                        gen_push_normal(i, y, pBuf, &movecount);
+                    y = i + 17;
+                    if (y < 64 && col < 7 && color[y] != current_side)
+                        gen_push_normal(i, y, pBuf, &movecount);
+                break ;
+                case KING:
+                    col = COL(i);
+                    if (col && color[i - 1] != current_side) // left
+                        gen_push_normal(i, i - 1, pBuf, &movecount);
+                    if (col < 7 && color[i + 1] != current_side) // right 
+                        gen_push_normal(i, i + 1, pBuf, &movecount);
+                    if (i > 7 && color[i - 8] != current_side) // up 
+                        gen_push_normal(i, i - 8, pBuf, &movecount);
+                    if (i < 56 && color[i + 8] != current_side) // down 
+                        gen_push_normal(i, i + 8, pBuf, &movecount);
+                    if (col && i > 7 && color[i - 9] != current_side) // left up 
+                        gen_push_normal(i, i - 9, pBuf, &movecount);
+                    if (col < 7 && i > 7 && color[i - 7] != current_side) // right up 
+                        gen_push_normal(i, i - 7, pBuf, &movecount);
+                    if (col && i < 56 && color[i + 7] != current_side) // left down  
+                        gen_push_normal(i, i + 7, pBuf, &movecount);
+                    if (col < 7 && i < 56 && color[i + 9] != current_side) // right down 
+                        gen_push_normal(i, i + 9, pBuf, &movecount);
+                break ;
+                default: 
+                    puts("Unknown Chess Piece!");
+                    assert(0);   
+            }
+        }
+    }
+    return movecount;
+}
+
+
 void init_board(void)
 {
     memcpy(board, initial_board, sizeof(board));
@@ -45,8 +233,15 @@ int main()
     side = WHITE;
     computer_side = BLACK;
     init_board();
-    while (1)
+    for (;;)
     {
+        if (side == computer_side) // Computers Turn
+        {
+            // Find best move to react to current position
+           // MOVE    best_move = computer_think(max_depth); !IMPLEMENT LATER
+           // make_move(best_move);
+            continue ;
+        }
         // Get user input 
         printf(">> ");
         if (scanf("%s", user_input) == EOF)
@@ -61,6 +256,23 @@ int main()
             printf("Goodbye, thankyou for playing\n");
             return 0;
         }
+        // now maybe the user has entered the move ? - parse values 
+        // Extract source square
+        from = user_input[0] - 'a'; //  - number between 0 - 7 / col 
+        from += 8 * (8 - (user_input[1] - '0')); // - number between 1 - 8 / row | combined to get full square address
+
+        // Extract Dest Square 
+        dest = user_input[2] - 'a';
+        dest += 8 * (8 - (user_input[3] - '0'));
+
+        // Print Extracted squares 
+        printf("\nfrom: %d to: %d \n", from, dest);
+
+        // Lets set ply to zero and generate all the moves 
+        ply = 0;
+        move_count = generate_all_moves(side, moveBuffer);
+
+        printf("\n move_counts generated : %d\n", move_count);
     }
 
 }
--- CL_Chess.h
+++ CL_Chess.h
@@ -27,6 +27,30 @@
 
 #define MATE            10000 // Equivalent to king, losing king == mate
 
+#define COL(pos)        ((pos)&7)
+#define ROW(pos)        (((unsigned)pos)>>3)
+
+// Some Board Constants for ease of reading 
+
+#define ONE_RANK (8)
+#define TWO_RANKS (16)
+#define UP (-8)
+#define DOWN (8)
+#define LEFT (-1)
+#define RIGHT (1)
+#define RIGHT_UP (RIGHT+UP)
+#define LEFT_UP (LEFT+UP)
+#define RIGHT_DOWN (RIGHT+DOWN)
+#define LEFT_DOWN (LEFT+DOWN)
+
+// For Generating Moves 
+#define MOVE_TYPE_NONE                      0
+#define MOVE_TYPE_NORMAL                    1
+#define MOVE_TYPE_PROMOTION_TO_QUEEN        4
+#define MOVE_TYPE_PROMOTION_TO_ROOK         5
+#define MOVE_TYPE_PROMOTION_TO_BISHOP       6
+#define MOVE_TYPE_PROMOTION_TO_KNIGHT       7
+
 // Board Representation 
 
 int         initial_board[64] = 
@@ -41,10 +65,6 @@ int         initial_board[64] =
     ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK,
 };
 
-int     board[64];
-int     color[64];
-int     side; // Side to move [Black or White]
-int     computer_side = -1;
 
 int     initial_color[64] =
 {
@@ -58,6 +78,14 @@ int     initial_color[64] =
     WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE,
 };
 
+int     board[64];
+int     color[64];
+int     side; // Side to move [Black or White]
+int     computer_side = -1;
+int     max_depth = 5;
+
+
+
 // STRUCTS
 
 typedef struct s_move
@@ -66,4 +94,24 @@ typedef struct s_move
     int     dest;
     int     type;
 }       MOVE;
+
+typedef struct s_history // move history
+{
+    MOVE    m;
+    int     cap;
+}   HISTORY;
+
+/* For Searching */
+int     nodes; // Count of all visited nodes for seaching 
+int     ply;  
+/*
+    a "ply" refers to one move made by one player, and a "two-ply search" would involve considering two moves ahead, 
+    i.e., both the player's move and the opponent's response. In the context of computer science, 
+    "ply" may also refer to the depth of a search algorithm or decision tree, where each ply represents a level of analysis or computation.
+*/
+
+/* For Storing */
+
+HISTORY hist[6000];  // Game length < 6000
+int     hdp;         // Current move order 
 
